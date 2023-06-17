f53599ec3b0af8d62c49cc8cb6df80c27514f66a
Shiraku Yanai <shirakuyanai@gmail.com>
2023-06-17 18:09:59 +0700

    fix register code pasted into login controller

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -7,22 +7,24 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Security.Cryptography;
 using System.Text;
+using System.Linq;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using server.Controllers;
 
 namespace server.Controllers
 {
-
-    public class RegisterController : Controller
+    public class LoginController : BaseController
     {
-        
-        private readonly serverContext _context;
-
-        public RegisterController(serverContext context)
+        public LoginController(serverContext context, IHttpContextAccessor httpContextAccessor) : base(context, httpContextAccessor)
         {
-            _context = context;
+
         }
-        
+
         [HttpGet]
-        [Route("Register")]
+        [Route("Login")]
         public IActionResult Index()
         {
             var message = TempData["Message"] as string;
@@ -35,74 +37,121 @@ namespace server.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        [Route("Register")]
-        public async Task<IActionResult> Register([Bind("Username,Password,Email,FirstName,LastName,Phone,Role,Status")] User user)
+        public IActionResult Login(string username, string password)
         {
-            if (ModelState.IsValid)
+            var user = _context.User.FirstOrDefault(u => u.Username == username);
+            if (user != null && VerifyPassword(password, user.Password))
             {
-                string id;
-                bool duplication;
-
-                do
-                {
-                    id = Guid.NewGuid().ToString();
-                    duplication = _context.User.Any(u => u.Uid == id);
-                }
-                while (duplication);
-
-                if (_context.User.Any(u => u.Email == user.Email))
-                {
-                    TempData["msg"] = "Email already taken.";
-                    return RedirectToAction("Index", "Register");
-                }
-                else if (_context.User.Any(u => u.Username == user.Username))
-                {
-                    TempData["msg"] = "Username already taken.";
-                    return RedirectToAction("Index", "Register");
-                }
-
-                else{
-                    string hashedPassword = HashPassword(user.Password);
-                    user.Password = hashedPassword;
-                    user.Uid = id;
-                    
-                    _context.Add(user);
-                    await _context.SaveChangesAsync();
-                    TempData["msg"] = "Registration succeeded!";
-                    return RedirectToAction("Index", "Register");
-                }
+                var tokenString = GenerateJwtToken(user);
+                HttpContext.Session.SetString("JwtToken", tokenString);
+                return RedirectToAction("Index", "Home");
             }
-            else
-            {
-                // Registration failed
-                var errorMessages = new List<string>();
-                foreach (var modelStateValue in ModelState.Values)
-                {
-                    foreach (var error in modelStateValue.Errors)
-                    {
-                        errorMessages.Add(error.ErrorMessage);
-                    }
-                }
-                TempData["msg"] = errorMessages[0];
 
-                return RedirectToAction("Index", "Register");
-            }
+            // Login failed
+            // You can handle the failed login attempt here, such as displaying an error message on the login page.
+            TempData["Error"] = "Invalid username or password";
+            return RedirectToAction("Index", "Login");
         }
 
-        private string HashPassword(string password)
+        private bool VerifyPassword(string enteredPassword, string storedPassword)
         {
-            byte[] salt;
-            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+            // Decode the Base64-encoded stored password to get the byte array
+            byte[] hashedBytes = Convert.FromBase64String(storedPassword);
 
-            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000);
-            byte[] hash = pbkdf2.GetBytes(20);
+            // Extract the salt from the hashed password
+            byte[] salt = new byte[16];
+            Array.Copy(hashedBytes, 0, salt, 0, 16);
 
-            byte[] hashBytes = new byte[36];
-            Array.Copy(salt, 0, hashBytes, 0, 16);
-            Array.Copy(hash, 0, hashBytes, 16, 20);
+            // Hash the entered password with the same salt
+            var pbkdf2 = new Rfc2898DeriveBytes(enteredPassword, salt, 10000);
+            byte[] enteredHash = pbkdf2.GetBytes(20);
 
-            return Convert.ToBase64String(hashBytes);
+            // Compare the entered hash with the stored hash
+            for (int i = 0; i < 20; i++)
+            {
+                if (hashedBytes[i + 16] != enteredHash[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
+        public User GetUserFromJwtToken(string tokenString)
+        {
+            // Verify and decode the JWT token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("slkdjapowue1-qopwasjd012hueboiqasklncbvoilwjrnb`92wk`j1ni2w8qs9pd"));
+
+            var validationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = key,
+                ValidateIssuer = true,
+                ValidIssuer = "your-issuer",
+                ValidateAudience = true,
+                ValidAudience = "your-audience",
+            };
+
+            SecurityToken validatedToken;
+
+            var principal = tokenHandler.ValidateToken(tokenString, validationParameters, out validatedToken);
+            // Extract user information from the token claims
+            var userId = principal.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            var username = principal.FindFirst(ClaimTypes.Name).Value;
+
+            // Create a user object with the extracted information
+            var found_user = _context.User.FirstOrDefault(u => u.Uid == userId);
+            var user = new User
+            {
+                Uid = userId,
+                Username = username,
+                FirstName = found_user.FirstName,
+                LastName = found_user.LastName,
+                Email = found_user.Email,
+                Phone = found_user.Phone,
+            };
+
+            return user;
+        }
+
+        public string GenerateJwtToken(User user)
+        {
+            // Create claims for the user
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Uid),
+                new Claim(ClaimTypes.Name, user.Username),
+            };
+
+            // Generate a security key
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("slkdjapowue1-qopwasjd012hueboiqasklncbvoilwjrnb`92wk`j1ni2w8qs9pd"));
+
+            // Generate a signing credential using the key
+            var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+            // Create a JWT token
+            var token = new JwtSecurityToken(
+                issuer: "your-issuer",
+                audience: "your-audience",
+                claims: claims,
+                expires: DateTime.Now.AddDays(1), // Token expiration time
+                signingCredentials: signingCredentials
+            );
+
+            // Serialize the token to a string
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return tokenString;
+        }
+
+        [HttpGet("/logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("JwtToken");
+            return Redirect("/login");
+        }
     }
 }
