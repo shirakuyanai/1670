0bf84b69598e5ac87405493d08955a012f6eeab1
Shiraku Yanai <shirakuyanai@gmail.com>
2023-06-18 20:30:55 +0700

    changed layout template

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -78,45 +78,6 @@ namespace server.Controllers
             return true;
         }
 
-        public User GetUserFromJwtToken(string tokenString)
-        {
-            // Verify and decode the JWT token
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("slkdjapowue1-qopwasjd012hueboiqasklncbvoilwjrnb`92wk`j1ni2w8qs9pd"));
-
-            var validationParameters = new TokenValidationParameters
-            {
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = key,
-                ValidateIssuer = true,
-                ValidIssuer = "your-issuer",
-                ValidateAudience = true,
-                ValidAudience = "your-audience",
-            };
-
-            SecurityToken validatedToken;
-
-            var principal = tokenHandler.ValidateToken(tokenString, validationParameters, out validatedToken);
-            // Extract user information from the token claims
-            var userId = principal.FindFirst(ClaimTypes.NameIdentifier).Value;
-
-            var username = principal.FindFirst(ClaimTypes.Name).Value;
-
-            // Create a user object with the extracted information
-            var found_user = _context.User.FirstOrDefault(u => u.Uid == userId);
-            var user = new User
-            {
-                Uid = userId,
-                Username = username,
-                FirstName = found_user.FirstName,
-                LastName = found_user.LastName,
-                Email = found_user.Email,
-                Phone = found_user.Phone,
-            };
-
-            return user;
-        }
-
         public string GenerateJwtToken(User user)
         {
             // Create claims for the user
