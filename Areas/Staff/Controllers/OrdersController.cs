037e7f8e2f813b2679888e64cb865a154f7d0d92
AnhTungMang <tungnqsgch210015@fpt.edu.vn>
2023-06-22 09:40:52 +0700

    feat edit order

--- Areas/Staff/Controllers/OrdersController.cs
+++ Areas/Staff/Controllers/OrdersController.cs
@@ -5,166 +5,101 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using server.Models;
+using server.Controllers;
 using server.Data;
+using server.Models;
 
 namespace server.Areas.Staff.Controllers
 {
-    [Area("Staff")]
-    public class OrdersController : Controller
-    {
-        private readonly serverContext _context;
+	[Area("Staff")]
+	public class OrdersController : BaseController
+	{
+		public OrdersController(serverContext context, IHttpContextAccessor httpContextAccessor) : base(context, httpContextAccessor)
+		{
+			
+		}
 
-        public OrdersController(serverContext context)
-        {
-            _context = context;
-        }
+		// GET: Staff/Orders
+		public async Task<IActionResult> Index()
+		{
+			if (this_user == null)
+			{
+				return Redirect("/");
+			}
+			if (this_user.Role != 2)
+			{
+				return Redirect("/");
+			}
+			ViewData["User"] = this_user;
+			var serverContext = _context.Order.Include(o => o.Address).Include(o => o.User);
+			return View(await serverContext.ToListAsync());
+		}
 
-        // GET: Staff/Orders
-        public async Task<IActionResult> Index()
-        {
-            var serverContext = _context.Order.Include(o => o.Address).Include(o => o.User);
-            return View(await serverContext.ToListAsync());
-        }
 
-        // GET: Staff/Orders/Details/5
-        public async Task<IActionResult> Details(string? id)
-        {
-            if (id == null || _context.Order == null)
-            {
-                return NotFound();
-            }
 
-            var order = await _context.Order
-                .Include(o => o.Address)
-                .Include(o => o.User)
-                .FirstOrDefaultAsync(m => m.Order_id == id);
-            if (order == null)
-            {
-                return NotFound();
-            }
+		// GET: Staff/Orders/Edit/5
+		public async Task<IActionResult> Edit(string id)
+		{
+			if (this_user == null)
+			{
+				return Redirect("/");
+			}
+			if (this_user.Role != 2)
+			{
+				return Redirect("/");
+			}
+			ViewData["User"] = this_user;
+			if (id == null || _context.Order == null)
+			{
+				return NotFound();
+			}
 
-            return View(order);
-        }
+			var order = await _context.Order.FindAsync(id);
+			if (order == null)
+			{
+				return NotFound();
+			}
+			ViewData["Address_id"] = new SelectList(_context.Address, "Address_id", "Address_id", order.Address_id);
+			ViewData["Uid"] = new SelectList(_context.User, "Uid", "Uid", order.Uid);
+			return View(order);
+		}
 
-        // GET: Staff/Orders/Create
-        public IActionResult Create()
-        {
-            ViewData["Address_id"] = new SelectList(_context.Address, "Address_id", "Address_id");
-            ViewData["Uid"] = new SelectList(_context.User, "Uid", "Uid");
-            return View();
-        }
+		// POST: Staff/Orders/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(string id, [Bind("Order_id,Uid,Address_id,Created_at,Updated_at,Status,Total")] Order order)
+		{
+			if (id != order.Order_id)
+			{
+				return NotFound();
+			}
 
-        // POST: Staff/Orders/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Order_id,Uid,Address_id,Created_at,Updated_at,Status,Total")] Order order)
-        {
-                _context.Add(order);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-        }
+			try
+			{
+				_context.Update(order);
+				await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!OrderExists(order.Order_id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
 
-        // GET: Staff/Orders/Edit/5
-        public async Task<IActionResult> Edit(string? id)
-        {
-            if (id == null || _context.Order == null)
-            {
-                return NotFound();
-            }
-
-            var order = await _context.Order.FindAsync(id);
-            if (order == null)
-            {
-                return NotFound();
-            }
-            ViewData["Address_id"] = new SelectList(_context.Address, "Address_id", "Address_id", order.Address_id);
-            ViewData["Uid"] = new SelectList(_context.User, "Uid", "Uid", order.Uid);
-            return View(order);
-        }
-
-        // POST: Staff/Orders/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("Order_id,Updated_at,Status")] Order order)
-        {
-
-            if (order.Status != null)
-            {
-                try
-                {
-                    var foundOrder = _context.Order.AsNoTracking().FirstOrDefault(o => o.Order_id == order.Order_id);
-                    order.Address_id = foundOrder.Address_id;
-                    order.Uid = foundOrder.Uid;
-                    order.Created_at = foundOrder.Created_at;
-                    order.Updated_at = DateTime.Now;
-                    order.Total = foundOrder.Total;
-
-                    _context.Update(order);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!OrderExists(order.Order_id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(order);
-        }
-
-        // GET: Staff/Orders/Delete/5
-        public async Task<IActionResult> Delete(string? id)
-        {
-            if (id == null || _context.Order == null)
-            {
-                return NotFound();
-            }
-
-            var order = await _context.Order
-                .Include(o => o.Address)
-                .Include(o => o.User)
-                .FirstOrDefaultAsync(m => m.Order_id == id);
-            if (order == null)
-            {
-                return NotFound();
-            }
-
-            return View(order);
-        }
-
-        // POST: Staff/Orders/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
-        {
-            if (_context.Order == null)
-            {
-                return Problem("Entity set 'serverContext.Order'  is null.");
-            }
-            var order = await _context.Order.FindAsync(id);
-            if (order != null)
-            {
-                _context.Order.Remove(order);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool OrderExists(string id)
-        {
-          return (_context.Order?.Any(e => e.Order_id == id)).GetValueOrDefault();
-        }
-    }
+			ViewData["Address_id"] = new SelectList(_context.Address, "Address_id", "Address_id", order.Address_id);
+			ViewData["Uid"] = new SelectList(_context.User, "Uid", "Uid", order.Uid);
+			return Redirect("/staff/orders");
+		}
+		private bool OrderExists(string id)
+		{
+			return (_context.Order?.Any(e => e.Order_id == id)).GetValueOrDefault();
+		}
+	}
 }
